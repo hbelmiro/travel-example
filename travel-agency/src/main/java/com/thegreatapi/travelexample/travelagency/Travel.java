7032ba03fcef7168e8385b0f991f2702537a5747
hbelmiro <helber.belmiro@gmail.com>
2022-03-10 21:29:10 -0300

    Created Travel class (work in progress)
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

---
+++ travel-agency/src/main/java/com/thegreatapi/travelexample/travelagency/Travel.java
@@ -0,0 +1,10 @@
+package com.thegreatapi.travelexample.travelagency;
+
+public class Travel {
+
+    private String destination;
+    private String departureDate;
+    private String returnDate;
+    private String customerName;
+    private String customerEmail;
+}
